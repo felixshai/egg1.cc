fd1e389720b113ab053573c21f5612a32fc8493d
Merlin Helix <s22407707@gmail.com>
2015-03-11 11:46:07 +0800

    TEST II

--- my_first_try.cpp
+++ my_first_try.cpp
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
 	printf("hi \n");
-	printf("and this is for testing the tree.");
+	printf("and this is for testing the tree.\n");
+	printf("and this is for testing the tree. TestII for real. \n");
 	system("pause");
-	
 	
 	
 	return 0;
