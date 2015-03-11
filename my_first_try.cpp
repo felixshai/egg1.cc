1261f2a01a4b67b5b3e370a66cba9df45a657470
Merlin Helix <s22407707@gmail.com>
2015-03-11 11:45:14 +0800

    test for commit

---
+++ my_first_try.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hi \n");
+	printf("and this is for testing the tree.");
+	system("pause");
+	
+	
+	
+	return 0;
+}
